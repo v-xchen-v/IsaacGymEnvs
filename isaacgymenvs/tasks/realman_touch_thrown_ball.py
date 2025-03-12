0b96a33e8c3af480279dada7ae807e503338b219
Xi Chen <407081305@qq.com>
2025-03-12 16:47:20 +0800

    load robot asset

--- isaacgymenvs/tasks/realman_touch_thrown_ball.py
+++ isaacgymenvs/tasks/realman_touch_thrown_ball.py
@@ -1,4 +1,5 @@
 from isaacgym import gymapi
+from isaacgym import gymutil, gymtorch, gymapi
 from isaacgymenvs.tasks.base.vec_task import VecTask
 import torch
 import numpy as np
@@ -20,7 +21,7 @@ class RealmanTouchThrownBall(VecTask):
         # self.max_episode_length = self.cfg["env"]["maxEpisodeLength"]
     
         # Create environments
-        self.create_sim()
+        # self.create_sim() # do not call this here, it is called in the parent class
         
         # # Set observation and action spaces
         # self.num_observations = 
@@ -29,6 +30,9 @@ class RealmanTouchThrownBall(VecTask):
         self.obs_buf = torch.zeros((self.num_envs, self.num_observations), device=self.device)
         self.rewards = torch.zeros(self.num_envs, device=self.device)
         self.reset_buf = torch.ones(self.num_envs, device=self.device).long()
+        
+        dof_state_tensor = self.gym.acquire_dof_state_tensor(self.sim)
+        self.dof_state = gymtorch.wrap_tensor(dof_state_tensor)
         
     def create_sim(self):
         # set the up axis to be z-up given that assets are y-up by default
@@ -53,12 +57,49 @@ class RealmanTouchThrownBall(VecTask):
         env_upper = gymapi.Vec3(spacing, spacing, spacing)
         
         asset_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../assets")
-        asset_file = "urdf/cartpole.urdf"
+        asset_file = "urdf/realman/rm_75_6f_description.urdf"
         
-        # Create environments
+        if "asset" in self.cfg["env"]:
+            asset_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.cfg["env"]["asset"].get("assetRoot", asset_root))
+            asset_file = self.cfg["env"]["asset"].get("assetFileName", asset_file)
+
+        asset_path = os.path.join(asset_root, asset_file)
+        asset_root = os.path.dirname(asset_path)
+        asset_file = os.path.basename(asset_path)
+
+        asset_options = gymapi.AssetOptions()
+        asset_options.fix_base_link = True
+        robot_asset = self.gym.load_asset(self.sim, asset_root, asset_file, asset_options)
+        # self.num_dof = self.gym.get_asset_dof_count(robot_asset)
+        
+        pose = gymapi.Transform()
+        if self.up_axis == 'z':
+            pose.p.z = 2.0
+            # asset is rotated z-up by default, no additional rotations needed
+            pose.r = gymapi.Quat(0.0, 0.0, 0.0, 1.0)
+        else:
+            pose.p.y = 2.0
+            pose.r = gymapi.Quat(-np.sqrt(2)/2, 0.0, 0.0, np.sqrt(2)/2)
+
+        # self.robot_handles = []
+        # self.envs = []
+        # # Create environments
         for i in range(self.num_envs):
-            # Add environment setup code here
-            pass
+        #     # Add environment setup code here
+            env_ptr = self.gym.create_env(
+                self.sim, env_lower, env_upper, num_per_row
+            )
+            robot_handle = self.gym.create_actor(env_ptr, robot_asset, pose, "realman", i, 1, 0)
+
+        #     dof_props = self.gym.get_actor_dof_properties(env_ptr, robot_handle)
+        #     dof_props['driveMode'][:] = gymapi.DOF_MODE_POS
+        #     dof_props['stiffness'][:] = 400.0
+        #     dof_props['damping'][:] = 80.0
+        #     self.gym.set_actor_dof_properties(env_ptr, robot_handle, dof_props)
+
+        #     self.envs.append(env_ptr)
+        #     self.robot_handles.append(robot_handle)
+
             
     def reset_idx(self, env_ids):
         # Reset logic for specified environments
